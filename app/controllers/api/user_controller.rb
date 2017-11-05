cb0ca5c5e7f05ae5a04a6442dca70342f46cfc68
MrDSGC <dcheng47@gmail.com>
2017-11-04 21:06:23 -0700

    routes and controller for user

---
+++ app/controllers/api/user_controller.rb
@@ -0,0 +1,19 @@
+class Api::UserController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      redner 'api/users/show'
+    else
+      render json:
+      @user.errors.full_messages, status:422
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password,:biography, :profile_pic_url)
+  end
+  
+end
